1abe8bec440d7bdb9d98635c714c0ebc751ae20f
ElSenorRapido <stephenquick45@gmail.com>
2025-04-10 13:56:17 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
+import os
 
 app = Flask(__name__)
 
@@ -44,4 +45,5 @@ def get_scores():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
